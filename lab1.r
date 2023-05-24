712d1a5197ebe3ffbded1a12fcd06bc242936195
yuriyhryshman <yuriyhryshman@gmail.com>
2023-05-24 05:22:30 +0200

    first lab

---
+++ lab1.r
@@ -0,0 +1,11 @@
+vector <- seq(45, 123, by = 3)
+result_mean = mean(vector)
+
+my_matrix <- matrix(data = dataVec, nrow = 3, ncol = 3)
+print(my_matrix[1:1, 2:3])
+
+my_list <- list(name = "Yurii", age = 22, location = "Vienna")
+
+print(my_list$name)
+print(my_list$age)
+print(my_list$location)
